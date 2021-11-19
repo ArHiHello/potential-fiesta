572864a73a3c0ee7fdd04489298d4b36a8f0a2ad
ArHiHello <80757218+ArHiHello@users.noreply.github.com>
2021-11-19 14:24:02 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import discord
 from discord.ext import commands
+from websever import keep_alive
 
 client = commands.Bot(command_prefix='?')
 
@@ -7,5 +8,6 @@ client.command()
 async def hi(ctx):
   await ctx.send('HELLO')
 
-
+keep_alive()
+  
 client.run('ODU1MTM2NTI5NDk1ODgzODI3.YMuF9A.27-z-UWzMlN0x2kFILja5uJoudw')
