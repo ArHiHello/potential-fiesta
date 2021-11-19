18533536a56f25254414b6e3c42971c1d5dc9c26
ArHiHello <80757218+ArHiHello@users.noreply.github.com>
2021-11-19 14:19:05 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+import discord
+from discord.ext import commands
+
+client = commands.Bot(command_prefix='?')
+
+client.command()
+async def hi(ctx):
+  await ctx.send('HELLO')
+
+
+client.run('ODU1MTM2NTI5NDk1ODgzODI3.YMuF9A.27-z-UWzMlN0x2kFILja5uJoudw')
